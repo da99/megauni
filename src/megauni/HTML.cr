5adf9946dc0921c9d3d1f58800752f1ac005d8bc
da99 <i-hate-spam-1234567@mailinator.com>
2018-03-09 23:25:51 -0600

    Started: importing code from megauni.alpha

--- src/megauni/HTML.cr
+++ src/megauni/HTML.cr
@@ -54,47 +54,124 @@ module MEGAUNI
       ]
     end
 
-    def h1
-      raw! "<h1>"
-      text? {
-        with self yield
-      }
-      raw! "</h1>"
-    end
+    {% for x in %w[h1 h2 h3].map(&.id) %}
+      def {{x}}(*args, **names)
+        tag("{{x}}", *args, **names) do
+          text? {
+            with self yield
+          }
+        end
+      end
+    {% end %}
 
-    def form
-      raw! "<form>"
-      text? { with self yield }
-      raw! "</form>"
+    def form(*args, **names)
+      tag("form", *args, **names) {
+        text? { with self yield }
+      }
     end # === def form
 
-    def button
-      raw! "<button>"
-      text? { with self yield }
-      raw! "</button>"
+    def fieldset
+      tag("fieldset") {
+        with self yield self
+      }
+    end
+
+    def label
+      tag("label") {
+        text? { with self yield self }
+      }
+    end
+
+    def meta(**args)
+      inspect! args
+      tag("meta") #, **args)
+    end
+
+    def header
+      tag("header") {
+        with self yield self
+      }
+    end
+
+    def section
+      tag("section") {
+        with self yield self
+      }
+    end
+
+    def button(*args)
+      tag("button", *args) {
+        with self yield
+      }
     end # === def button
 
     def screen_name_input
-      raw! "<input"
-      attr! :name, "screen_name"
-      attr! :type, "text"
-      attr! :minlength, MEGAUNI::MIN_SCREEN_NAME.to_s
-      attr! :maxlength, MEGAUNI::MAX_SCREEN_NAME.to_s
-      attr! :value, ""
-      attr! :required
-      raw '>'
+      tag(
+        "input",
+        :required,
+        name:      "screen_name",
+        type:      "text",
+        minlength: MEGAUNI::MIN_SCREEN_NAME.to_s,
+        maxlength: MEGAUNI::MAX_SCREEN_NAME.to_s,
+        value:     ""
+      )
     end
 
+    def input_text(name : String, value : String)
+      tag("input", name: name, type: "text", value: "")
+    end # === def input
+
+    def input_password(name : String)
+      tag("input", name: name, type: "password", value: "")
+    end # === def input
+
+    def template(**names)
+      tag("template", **names) {
+        with self yield self
+      }
+    end # === def template
+
+    def span(*args, **names)
+      tag("span", *args, **names) {
+        text? { with self yield self }
+      }
+    end # === def span
+
+    def nav
+      tag("nav") {
+        with self yield self
+      }
+    end # === def nav
+
+    def button(*args, **names)
+      tag("button", *args, **names) {
+        text? { with self yield self }
+      }
+    end # === def button
+
+    def div(*args, **names)
+      tag("div", *args, **names) {
+        text? { with self yield self }
+      }
+    end # === def div
+
     def pass_phrase_input(prefix : String? = nil)
-      raw! "<input"
-      attr! :name, "#{prefix == "" ? "" : "#{prefix}_"}pass_phrase"
-      attr! :type, "password"
-      attr! :minlength, MEGAUNI::MIN_PASS_PHRASE.to_s
-      attr! :maxlength, MEGAUNI::MAX_PASS_PHRASE.to_s
-      attr! :value, ""
-      attr! :required
-      raw '>'
+      tag(
+        "input",
+        :required,
+        name:      "#{prefix == "" ? "" : "#{prefix}_"}pass_phrase",
+        type:      "password",
+        minlength: MEGAUNI::MIN_PASS_PHRASE.to_s,
+        maxlength: MEGAUNI::MAX_PASS_PHRASE.to_s,
+        value:     ""
+      )
     end # === def to_html
+
+    def script(src : String)
+      tag("script", type: "text/javascript", src: src) {
+        with self yield self
+      }
+    end # === def script
 
   end # === class HTML
 
--- src/megauni/Stranger_Root/__.cr
+++ src/megauni/Stranger_Root/__.cr
@@ -38,24 +38,195 @@ module MEGAUNI
     def root
       route.html!(
         ::MEGAUNI::HTML.to_html do
+          doctype!
           html {
             head {
               utf8!
               script!
               stylesheet!
-              title { "Home" }
+              title { "#{MEGAUNI.site_name}: Home" }
+              meta(name: "Description", content: "Sign-In or Create an Account if you don't have one.")
             }
             body {
-              h1 { MEGAUNI.site_name }
-              div(".body") {
-                form {
-                  button { "Create Account" }
-                }
+              header {
+                h1 { MEGAUNI.site_name }
               }
-            }
-          }
+              section {
+                h2 { "Log-In" }
+                form {
+                  fieldset {
+                    label { "Screen Name" }
+                    screen_name_input
+                  }
+
+                  fieldset {
+                    label {
+                      span { "Pass Phrase" }
+                      span(class: "sub") { "(spaces allowed)" }
+                    }
+                    pass_phrase_input
+                  }
+
+                  nav {
+                    button(class: "submit") { "Log-In" }
+                  }
+                } # === form
+              } # === section
+
+              section {
+                h2 { "Create A New Account" }
+                form {
+                  fieldset {
+                    label { "Screen Name" }
+                    screen_name_input
+                  }
+
+                  fieldset {
+                    label {
+                      span { "Pass Phrase" }
+                      span(class: "sub") { "(spaces allowed)" }
+                    }
+                    pass_phrase_input
+                  }
+
+                  fieldset {
+                    label { "Confirm Pass Phrase" }
+                    pass_phrase_input("confirm")
+                  }
+
+                  nav {
+                    button(class: "submit") { "Create" }
+                  }
+                } # === form
+              } # === section
+
+              div("#log_in", class: "block form") {
+                h3 { "Log-In" }
+                div(class: "content") {
+                  form(action: "/log-in", method: "post") {
+                    div(class: "fields") {
+                      div(class: "field screen_name") {
+                        label { "Screen name:" }
+                        input_text("screen_name", "")
+                      }
+                      div(class: "field passphrase") {
+                        label { "Pass phrase:" }
+                        input_password("pswd")
+                      }
+                      template(data_do: "template logged-in?") {
+                        div(class: "success_msg") { "You are now logged in. Loading..." }
+                      }
+
+                      template(data_do: "template FORM.error_msg") {
+                        div(class: "error_msg") { "{{msg}}" }
+                      }
+
+                      div(class: "field buttons", data_do: "hide FORM.submit") {
+                        button(class: "submit", data_do: "on_click send_msg FORM.submit") {
+                          "Log-In"
+                        }
+                      }
+                      div(class: "loading", data_do: "hide; show_if FORM.submit;    hide     FORM.is_response") {
+                        "Loading..."
+                      }
+                      div(class: "errors", data_do: "hide; show_if FORM.error_msg; print_if FORM.error_msg") {
+                      }
+                    } # === div
+                  } # === form
+                } # <!-- div.content -->
+              } # <!-- div.block.form -->
+
+              div(class: "block form") {
+                h3 { "Create a New Account" }
+                div(class: "content") {
+                  form(
+                    "#create_account",
+                    action: "/user",
+                    method: "POST",
+                    data_do: "on_send hide_button, on_respond_ok reset, wait_a_few, reload"
+                  ) {
+                    div(class: "fields") {
+                      div(class: "field screen_name") {
+                        label { "Screen name:" }
+                        input_text("screen_name", "")
+                      }
+
+                      div(class: "field pswd") {
+                        label {
+                          span(class: "main") { "Pass phrase" }
+                          span(class: "sub") { " (for better security, use spaces and words)" }
+                          span(class: "main") { ":" }
+                        }
+                        input_password("pswd")
+                      }
+
+                      div(class: "field confirm_pass_phrase") {
+                        label {
+                          span(class: "main") { "Re-type the pass phrase:" }
+                        }
+                        input_password("confirm_pass_word")
+                      }
+
+                      template(data_do: "template logged-in?") {
+                        div(class: "success_msg") { "You account has been created. Loading..." }
+                      }
+
+                      template(data_do: "template error-/user") {
+                        div(class: "error_msg") { "{{msg}}" }
+                      }
+
+                      div(class: "buttons") {
+                        button(class: "submit") { "Create Account" }
+                      }
+                    }
+                  }
+                }
+              } # === div
+
+              div("#intro", class: "block") {
+                h1(class: "site") {
+                  span(class: "main") { "mega" }
+                  span(class: "sub") { "UNI" }
+                }
+                p {
+                  a(href: "/home") { "/home" }
+                }
+                p {
+                  a(href: "/@da99") { "/@da99" }
+                }
+                p {
+                  a(href: "/!4567") { "/!4567" }
+                }
+                p {
+                  a(href: "/nowhere") { "/nowhere" }
+                }
+
+                div(class: "disclaimer") {
+                  p {
+                    text! "(c) 2012-"
+                    span(id: "copyright_year_today") { "2016" }
+                    text! "megauni.com. Some rights reserved."
+                  }
+                  p { "All other copyrights belong to their respective owners, who have no association to this site:" }
+                  p {
+                    span { "Logo font: " }
+                    a(href: "http://openfontlibrary.org/en/font/otfpoc") { "Aghja" }
+                  }
+                  p {
+                    span {
+                      text! "Palettes: "
+                      a(href: "http://www.colourlovers.com/lover/dvdcpd") { "dvdcpd" }
+                      a(href: "http://www.colourlovers.com/palette/154398/bedouin") { "shari_foru" }
+                    } # === span
+                  } # === div
+                }
+              } # div#intro
+
+            } # body
+            script("/applets/Browser/Megauni.js") { }
+          } # === html
         end
-      )
+      ) # === route.html!
     end # === def root
 
   end # === class Root
