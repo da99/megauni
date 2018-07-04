0460f83d941c557a15bab1f434157829ad85d433
da99 <da@bdrm>
2018-07-04 13:11:04 -0500

    Exported code to megauni_pg repo.

--- src/megauni/Model/Member/db/001-create.table.sql
+++
@@ -1,8 +0,0 @@
-
-
-CREATE TABLE IF NOT EXISTS "member" (
-  id                   BIGSERIAL PRIMARY KEY,
-  pswd_hash            BYTEA NOT NULL,
-  created_at           timestamptz NOT NULL DEFAULT NOW()
-);
-COMMIT;
