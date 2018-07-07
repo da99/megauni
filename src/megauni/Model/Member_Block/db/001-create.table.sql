fecaad2bf42c29ae6ff0d51a20cb6b5a8b02c86b
da99 <da@bdrm>
2018-07-06 19:02:47 -0500

    Development checkpoint.

--- src/megauni/Model/Member_Block/db/001-create.table.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS "member_block" (
-  id                   BIGSERIAL PRIMARY KEY,
-  screen_name_id       BIGINT NOT NULL,
-  member_id            BIGINT NOT NULL,
-  created_at           timestamptz NOT NULL DEFAULT NOW()
-);
